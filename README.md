e15fcdce69b2cc1cdcfa9e57faaf65ff68445071
Sam Jonston <johnstonsam712@gmail.com>
2024-02-01 16:53:59 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Basketball GUI Box Score
+
+A simple web app to record and display basketball box scores. The main goal is to make recording basketball box score easier, as most other similar software requires a learning curve to use. The user can simply click on the point of the basketball court and record the player's name, shot.
+
+This is by no means a tools for advance statistics, this is simply a replacement for a excel spreadsheet and also allow recording of positional shot that.
+
+# How to run
+
+1. Install `node.js`
+2. Run `npm install`
+3. Run `npm run css:build`
+
+Then server the contents of the directory on a server, such as `nginx` or `apache`.
---
+++ index.js
@@ -0,0 +1,297 @@
+'use strict'
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker@8.3.0?dts'
+
+const game_data = {
+  team1: {
+    players: []
+  },
+  team2: {
+    players: []
+  }
+}
+
+const court = document.getElementById('court')
+const svg = document.getElementById('court-svg')
+const missedShotMarkers = document.getElementById('missed-shot-markers')
+const clearMarkersButton = document.getElementById('clear-markers-button')
+const team1FinalScoreSpan = document.getElementById('team-1-final-score')
+const team2FinalScoreSpan = document.getElementById('team-2-final-score')
+
+const viewBox = svg.getAttribute('viewBox').split(' ')
+
+const svgViewBoxWidth = Number(viewBox[2])
+const svgViewBoxHeight = Number(viewBox[3])
+
+svg.addEventListener('click', function (event) {
+  console.log({
+    offsetX: event.offsetX,
+    offsetY: event.offsetY,
+    clientX: event.clientX,
+    clientY: event.clientY
+  })
+
+  const x = event.offsetX
+  const y = event.offsetY
+
+  const xScale = svg.clientWidth / svgViewBoxWidth
+  const yScale = svg.clientHeight / svgViewBoxHeight
+
+  const rectWidth = 10
+  const rectHeight = 10
+
+  const rectX = x / xScale - rectWidth / 2
+  const rectY = y / yScale - rectHeight / 2
+
+  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+  rect.setAttribute('x', rectX)
+  rect.setAttribute('y', rectY)
+  rect.setAttribute('height', 10)
+  rect.setAttribute('width', 10)
+  rect.setAttribute('fill', 'red')
+  missedShotMarkers.appendChild(rect)
+})
+
+clearMarkersButton.addEventListener('click', function () {
+  while (missedShotMarkers.firstChild) {
+    missedShotMarkers.removeChild(missedShotMarkers.firstChild)
+  }
+})
+
+window.addEventListener('keydown', function (event) {
+  //if key is backspace, remove from lastChild of missedShotMarkers
+  if (event.key === 'Backspace') {
+    if (missedShotMarkers.lastChild) {
+      missedShotMarkers.removeChild(missedShotMarkers.lastChild)
+    }
+  }
+})
+
+generateRandomBoxScore()
+
+team1FinalScoreSpan.textContent = game_data.team1.players.reduce(
+  (total, player) => total + player.points,
+  0
+)
+team2FinalScoreSpan.textContent = game_data.team2.players.reduce(
+  (total, player) => total + player.points,
+  0
+)
+
+function generateRandomBoxScore() {
+  const team1PlayerTbody = document.getElementById('team1-players-tbody')
+  const team2PlayerTbody = document.getElementById('team2-players-tbody')
+
+  //team 1 players
+  for (let index = 0; index < 13; index++) {
+    const team1Player = {}
+    const team2Player = {}
+
+    team1Player.number = randomUniqueNumber(
+      game_data.team1.players.map((p) => p.number),
+      30
+    )
+    team1Player.name = faker.person.fullName()
+    team1Player.points = randomWeightedNumber(25)
+    team1Player.assists = faker.number.int(5)
+    team1Player.rebounds = faker.number.int(10)
+    team1Player.fieldGoals = []
+    team1Player.freeThrows = []
+    team1Player.fouls = []
+    team1Player.minutes = randomWeightedNumber(40)
+
+    game_data.team1.players.push(team1Player)
+
+    team2Player.number = randomUniqueNumber(
+      game_data.team2.players.map((p) => p.number),
+      30
+    )
+    team2Player.name = faker.person.fullName()
+    team2Player.points = randomWeightedNumber(25)
+    team2Player.assists = faker.number.int(5)
+    team2Player.rebounds = faker.number.int(10)
+    team2Player.fieldGoals = []
+    team2Player.freeThrows = []
+    team2Player.fouls = []
+    team2Player.minutes = randomWeightedNumber(40)
+
+    game_data.team2.players.push(team2Player)
+
+    const team1Tr = document.createElement('tr')
+    const team1numberTd = document.createElement('td')
+    const team1nameTd = document.createElement('td')
+    const team1ptsTd = document.createElement('td')
+    const team1astTd = document.createElement('td')
+    const team1rebTd = document.createElement('td')
+    const team1twopaTd = document.createElement('td')
+    const team1twopmTd = document.createElement('td')
+    const team1threepaTd = document.createElement('td')
+    const team1threepmTd = document.createElement('td')
+
+    team1ptsTd.style.textAlign = 'right'
+    team1astTd.style.textAlign = 'right'
+    team1rebTd.style.textAlign = 'right'
+
+    team1numberTd.textContent = team1Player.number
+    team1nameTd.textContent = team1Player.name
+    team1ptsTd.textContent = team1Player.points
+    team1astTd.textContent = team1Player.assists
+    team1rebTd.textContent = team1Player.rebounds
+
+    team1Tr.appendChild(team1numberTd)
+    team1Tr.appendChild(team1nameTd)
+    team1Tr.appendChild(team1ptsTd)
+    team1Tr.appendChild(team1astTd)
+    team1Tr.appendChild(team1rebTd)
+    team1Tr.appendChild(team1twopaTd)
+    team1Tr.appendChild(team1twopmTd)
+    team1Tr.appendChild(team1threepaTd)
+    team1Tr.appendChild(team1threepmTd)
+    team1PlayerTbody.appendChild(team1Tr)
+
+    const team2Tr = document.createElement('tr')
+    const team2numberTd = document.createElement('td')
+    const team2nameTd = document.createElement('td')
+    const team2ptsTd = document.createElement('td')
+    const team2astTd = document.createElement('td')
+    const team2rebTd = document.createElement('td')
+    const team2twopaTd = document.createElement('td')
+    const team2twopmTd = document.createElement('td')
+    const team2threepaTd = document.createElement('td')
+    const team2threepmTd = document.createElement('td')
+
+    team2ptsTd.style.textAlign = 'right'
+    team2astTd.style.textAlign = 'right'
+    team2rebTd.style.textAlign = 'right'
+
+    team2numberTd.textContent = team2Player.number
+    team2nameTd.textContent = team2Player.name
+    team2ptsTd.textContent = team2Player.points
+    team2astTd.textContent = team2Player.assists
+    team2rebTd.textContent = team2Player.rebounds
+
+    team2Tr.appendChild(team2numberTd)
+    team2Tr.appendChild(team2nameTd)
+    team2Tr.appendChild(team2ptsTd)
+    team2Tr.appendChild(team2astTd)
+    team2Tr.appendChild(team2rebTd)
+    team2Tr.appendChild(team2twopaTd)
+    team2Tr.appendChild(team2twopmTd)
+    team2Tr.appendChild(team2threepaTd)
+    team2Tr.appendChild(team2threepmTd)
+    team2PlayerTbody.appendChild(team2Tr)
+  }
+
+  const team1Footertr = document.createElement('tr')
+  const team1FooternumberTd = document.createElement('td')
+  const team1FooternameTd = document.createElement('td')
+  const team1FooterptsTd = document.createElement('td')
+  const team1FooterastTd = document.createElement('td')
+  const team1FooterrebTd = document.createElement('td')
+  const team1FootertwopaTd = document.createElement('td')
+  const team1FootertwopmTd = document.createElement('td')
+  const team1FooterthreepaTd = document.createElement('td')
+  const team1FooterthreepmTd = document.createElement('td')
+
+  team1FooterptsTd.style.textAlign = 'right'
+  team1FooterastTd.style.textAlign = 'right'
+  team1FooterrebTd.style.textAlign = 'right'
+
+  team1FooternumberTd.style.fontWeight = 'bold'
+  team1FooterptsTd.style.fontWeight = 'bold'
+  team1FooterastTd.style.fontWeight = 'bold'
+  team1FooterrebTd.style.fontWeight = 'bold'
+
+  team1FooternumberTd.textContent = 'Total'
+
+  team1FooterptsTd.textContent = game_data.team1.players.reduce(
+    (total, player) => total + player.points,
+    0
+  )
+
+  team1FooterastTd.textContent = game_data.team1.players.reduce(
+    (total, player) => total + player.assists,
+    0
+  )
+
+  team1FooterrebTd.textContent = game_data.team1.players.reduce(
+    (total, player) => total + player.rebounds,
+    0
+  )
+
+  team1Footertr.appendChild(team1FooternumberTd)
+  team1Footertr.appendChild(team1FooternameTd)
+  team1Footertr.appendChild(team1FooterptsTd)
+  team1Footertr.appendChild(team1FooterastTd)
+  team1Footertr.appendChild(team1FooterrebTd)
+  team1Footertr.appendChild(team1FootertwopaTd)
+  team1Footertr.appendChild(team1FootertwopmTd)
+  team1Footertr.appendChild(team1FooterthreepaTd)
+  team1Footertr.appendChild(team1FooterthreepmTd)
+  team1PlayerTbody.appendChild(team1Footertr)
+
+  const team2Footertr = document.createElement('tr')
+  const team2FooternumberTd = document.createElement('td')
+  const team2FooternameTd = document.createElement('td')
+  const team2FooterptsTd = document.createElement('td')
+  const team2FooterastTd = document.createElement('td')
+  const team2FooterrebTd = document.createElement('td')
+  const team2FootertwopaTd = document.createElement('td')
+  const team2FootertwopmTd = document.createElement('td')
+  const team2FooterthreepaTd = document.createElement('td')
+  const team2FooterthreepmTd = document.createElement('td')
+
+  team2FooterptsTd.style.textAlign = 'right'
+  team2FooterastTd.style.textAlign = 'right'
+  team2FooterrebTd.style.textAlign = 'right'
+
+  team2FooternumberTd.style.fontWeight = 'bold'
+  team2FooterptsTd.style.fontWeight = 'bold'
+  team2FooterastTd.style.fontWeight = 'bold'
+  team2FooterrebTd.style.fontWeight = 'bold'
+
+  team2FooternumberTd.textContent = 'Total'
+
+  team2FooterptsTd.textContent = game_data.team2.players.reduce(
+    (total, player) => total + player.points,
+    0
+  )
+
+  team2FooterastTd.textContent = game_data.team2.players.reduce(
+    (total, player) => total + player.assists,
+    0
+  )
+
+  team2FooterrebTd.textContent = game_data.team2.players.reduce(
+    (total, player) => total + player.rebounds,
+    0
+  )
+
+  team2Footertr.appendChild(team2FooternumberTd)
+  team2Footertr.appendChild(team2FooternameTd)
+  team2Footertr.appendChild(team2FooterptsTd)
+  team2Footertr.appendChild(team2FooterastTd)
+  team2Footertr.appendChild(team2FooterrebTd)
+  team2Footertr.appendChild(team2FootertwopaTd)
+  team2Footertr.appendChild(team2FootertwopmTd)
+  team2Footertr.appendChild(team2FooterthreepaTd)
+  team2Footertr.appendChild(team2FooterthreepmTd)
+  team2PlayerTbody.appendChild(team2Footertr)
+}
+
+function randomWeightedNumber(maxNumber = 50) {
+  // Adjust the exponent to control the distribution
+  const exponent = 2
+
+  // Calculate the weighted random number
+  return Math.floor(Math.pow(Math.random(), exponent) * maxNumber) + 1
+}
+
+function randomUniqueNumber(currentNums, maxNumber = 99) {
+  let randomNumber = faker.number.int(maxNumber)
+  while (currentNums.includes(randomNumber)) {
+    randomNumber = faker.number.int(maxNumber)
+  }
+  return randomNumber
+}
+
+console.log(game_data)
