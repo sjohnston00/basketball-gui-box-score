442b5bb7ff697cc3c6b9b85e4c6bd41971dfea13
Sam Jonston <johnstonsam712@gmail.com>
2024-03-03 13:18:06 +0000

    add edit team page

--- src/players.ts
+++ src/players.ts
@@ -34,3 +34,16 @@ export async function deletePlayer(id: string) {
   await playersTable.removeItem(id)
 }
 
+export async function getPlayerById(playerId: string): Promise<Player | undefined> {
+  const player = (await playersTable.getItem(playerId)) as Record<string, any> | undefined
+  if (!player) return undefined
+
+  return {
+    key: playerId,
+    name: player.name,
+    number: player.number,
+    teamId: player.teamId,
+    createdAt: player.createdAt,
+  }
+}
+
